87427f984906055ef12d0f842cacf74df01d8599
Ferenc Böröczki <f3c0@users.noreply.github.com>
2019-06-28 23:32:49 +0200

    infinite money does not exist

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ This is an example solution of the following exercise:
 
 ## Running the program in Erlang shell
 
-   c(vending.erl),
-   Price = 12100,
-   Paid = 20000,
-   vending(Price, Paid).
+    c(vending),
+    Price = 12100,
+    Paid = 20000,
+    vending:test(Price, Paid).
 
-   [200,200,500,2000,5000]
+    {ok,[200,200,500,2000,5000]}
   
--- vending.erl
+++ vending.erl
@@ -1,17 +1,35 @@
 -module(vending).
 
--export([test/2]).
+-export([test/2, test/3]).
 
 test(Price, Paid) ->
-  Coins = [20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5],
+  Coins = [
+    {20000, 3},
+    {10000, 3},
+    {5000, 3},
+    {2000, 3},
+    {1000, 3},
+    {500, 3},
+    {200, 3},
+    {100, 3},
+    {50, 3},
+    {20, 3},
+    {10, 3},
+    {5, 3}
+  ],
+  test(Price, Paid, Coins).
+
+test(Price, Paid, Coins) ->
   Target = Paid - Price,
   change_back(Target, Coins, [], 0).
 
 change_back(Target, _Coins, CoinsBack, Sum) when Sum =:= Target ->
-  CoinsBack;
-change_back(Target, [Coin|Coins], CoinsBack, Sum) when Sum + Coin > Target ->
+  {ok, CoinsBack};
+change_back(_Target, [], CoinsBack, Sum) ->
+  {not_enough_coin, Sum, CoinsBack};
+change_back(Target, [{_Coin, 0} | Coins], CoinsBack, Sum) ->
   change_back(Target, Coins, CoinsBack, Sum);
-change_back(Target, [Coin|Coins], CoinsBack, Sum) ->
-  change_back(Target, [Coin|Coins], [Coin|CoinsBack], Sum + Coin).
-
-
+change_back(Target, [{Coin, _Count} | Coins], CoinsBack, Sum) when Sum + Coin > Target ->
+  change_back(Target, Coins, CoinsBack, Sum);
+change_back(Target, [{Coin, Count} | Coins], CoinsBack, Sum) ->
+  change_back(Target, [{Coin, Count - 1} | Coins], [Coin | CoinsBack], Sum + Coin).
